336aee4a626217e8239e23d39a4708d21e409de9
AzarovRoman <azarovrom9215@gmail.com>
2022-03-15 22:18:29 +0300

    fix stored procedure

--- MarvelousReportMicroService.DB/StoredProcedures/GetLeadByParameters.sql
+++ MarvelousReportMicroService.DB/StoredProcedures/GetLeadByParameters.sql
@@ -2,7 +2,8 @@
 	@Id integer = null,
 	@Name varchar(30) = null,
     @LastName varchar(30) = null,
-    @Birthdate date = null,
+    @StartBirthdate date = null,
+    @EndBirthdate date = null,
 	@Email varchar(30) = null,
 	@Phone varchar(20) = null,
 	@Role integer = null,
@@ -10,10 +11,11 @@
 as
 	select L.Id, L.[Name], L.LastName, L.BirthDate, L.Email, L.Phone, L.[Role], L.IsBanned from [dbo].[Lead] as L
 	where (@Id is null or @Id is not null and L.Id = @Id)
-	and (@Name is null or @Name is not null and L.[Name] = @Name)
-	and (@LastName is null or @LastName is not null and L.LastName = @LastName)
-	and (@BirthDate is null or @BirthDate is not null and L.BirthDate = @BirthDate)
-	and (@Email is null or @Email is not null and L.Email = @Email)
+	and (@Name is null or @Name is not null and L.[Name] like '%'+@Name+'%')
+	and (@LastName is null or @LastName is not null and L.LastName like '%'+@LastName+'%')
+	and (@StartBirthdate is null or @StartBirthdate is not null and L.BirthDate >= @StartBirthdate)
+	and (@EndBirthdate is null or @EndBirthdate is not null and L.BirthDate <= @EndBirthdate)
+	and (@Email is null or @Email is not null and L.Email like '%'+@Email+'%')
 	and (@Phone is null or @Phone is not null and L.Phone = @Phone)
 	and (@Role is null or @Role is not null and L.[Role] = @Role)
 	and (@IsBanned is null or @IsBanned is not null and L.IsBanned = @IsBanned)
