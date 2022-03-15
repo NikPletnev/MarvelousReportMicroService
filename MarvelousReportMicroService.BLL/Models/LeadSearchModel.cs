72a5b960ddc65872ef2a2d2d175230e8dcc41d94
AzarovRoman <azarovrom9215@gmail.com>
2022-03-15 22:23:52 +0300

    add models

---
+++ MarvelousReportMicroService.BLL/Models/LeadSearchModel.cs
@@ -0,0 +1,17 @@
+﻿using Marvelous.Contracts;
+
+namespace MarvelousReportMicroService.BLL.Models
+{
+    public class LeadSearchModel
+    {
+        public int? Id { get; set; }
+        public string? Name { get; set; }
+        public string? LastName { get; set; }
+        public DateTime? StartBirthDate { get; set; }
+        public DateTime? EndBirthDate { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public Role? Role { get; set; }
+        public bool? IsBanned { get; set; }
+    }
+}
--- MarvelousReportMicroService.DAL/Models/LeadSearch.cs
+++ MarvelousReportMicroService.DAL/Models/LeadSearch.cs
@@ -1,13 +1,14 @@
 ﻿using Marvelous.Contracts;
 
-namespace MarvelousReportMicroService.BLL.Models
+namespace MarvelousReportMicroService.DAL.Models
 {
-    public class LeadModelSearchRequest
+    public class LeadSearch
     {
         public int? Id { get; set; }
         public string? Name { get; set; }
         public string? LastName { get; set; }
-        public DateTime? BirthDate { get; set; }
+        public DateTime? StartBirthDate { get; set; }
+        public DateTime? EndBirthDate { get; set; }
         public string? Email { get; set; }
         public string? Phone { get; set; }
         public Role? Role { get; set; }
