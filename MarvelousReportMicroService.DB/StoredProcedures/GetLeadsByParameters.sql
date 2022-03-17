46222f6d7c0be516f27ff5e67db820a9666d787a
AzarovRoman <azarovrom9215@gmail.com>
2022-03-17 16:33:17 +0300

    has been corrected GetLeadsByParametres

--- MarvelousReportMicroService.DB/StoredProcedures/GetLeadsByParameters.sql
+++ MarvelousReportMicroService.DB/StoredProcedures/GetLeadsByParameters.sql
@@ -1,21 +1,35 @@
 ﻿create procedure [dbo].[GetLeadsByParameters]
 	@Id integer = null,
+
 	@Name varchar(30) = null,
+	@NameParam varchar(30) = null,
+
     @LastName varchar(30) = null,
+	@LastNameParam varchar(30) = null,
+
     @StartBirthdate date = null,
     @EndBirthdate date = null,
+
 	@Email varchar(30) = null,
+	@EmailParam varchar(30) = null,
+
 	@Phone varchar(20) = null,
+	@PhoneParam varchar(30) = null,
+
 	@Role integer = null,
 	@IsBanned integer = null
 as
 	select L.Id, L.[Name], L.LastName, L.BirthDate, L.Email, L.Phone, L.[Role], L.IsBanned from [dbo].[Lead] as L
 	where (@Id is null or @Id is not null and L.Id = @Id)
-	and (@Name is null or @Name is not null and L.[Name] like '%'+@Name+'%')
-	and (@LastName is null or @LastName is not null and L.LastName like '%'+@LastName+'%')
+
+	and (@Name is null or @Name is not null and L.[Name] like @NameParam)
+	and (@LastName is null or @LastName is not null and L.LastName like @LastNameParam)
+
 	and (@StartBirthdate is null or @StartBirthdate is not null and L.BirthDate >= @StartBirthdate)
 	and (@EndBirthdate is null or @EndBirthdate is not null and L.BirthDate <= @EndBirthdate)
-	and (@Email is null or @Email is not null and L.Email like '%'+@Email+'%')
-	and (@Phone is null or @Phone is not null and L.Phone = @Phone)
-	and (@Role is null or @Role is not null and L.[Role] = @Role)
+
+	and (@Email is null or @Email is not null and L.Email like @EmailParam)
+
+	and (@Phone is null or @Phone is not null and L.Phone like @PhoneParam)
+	and (@Role is null or @Role is not null and L.[Role] like @Role)
 	and (@IsBanned is null or @IsBanned is not null and L.IsBanned = @IsBanned)
--- MarvelousReportMicroService/Controllers/AccountsController.cs
+++ MarvelousReportMicroService/Controllers/AccountsController.cs
@@ -12,24 +12,23 @@ namespace MarvelousReportMicroService.API.Controllers
     [Route("api/[controller]")]
     public class AccountsController : Controller
     {
-
         private readonly ITransactionService _transactionService;
         private readonly IAccountService _accountService;
         private readonly IMapper _mapper;
-        private readonly Logger _logger;
+        private readonly ILogger<AccountsController> _logger;
 
-        public AccountsController(IMapper mapper, ITransactionService transactionService, IAccountService accountService)
+        public AccountsController(IMapper mapper, ILogger<AccountsController> logger, ITransactionService transactionService, IAccountService accountService)
         {
             _mapper = mapper;
             _accountService = accountService;
             _transactionService = transactionService;
-            _logger = NLog.Web.NLogBuilder.ConfigureNLog("NLog.config").GetCurrentClassLogger();
+            _logger = logger;
         }
 
         [HttpGet("{id}/balance/")]
         public ActionResult GetAccountBalance(int id)
         {
-            _logger.Info($"Запрос на получение баланса аккаунта с id = {id}");
+            _logger.LogInformation($"Запрос на получение баланса аккаунта с id = {id}");
             var balance = _accountService.GetAccountBalance(id);
             return Ok(balance);
         }
