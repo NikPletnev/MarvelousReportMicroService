ae82207951eb4d54a93e58010804e57ac7455c98
Nikita <klophah@gamil.com>
2022-03-22 18:06:22 +0300

    Used Kata

--- MarvelousReportMicroService.DAL/Repositories/LeadRepository.cs
+++ MarvelousReportMicroService.DAL/Repositories/LeadRepository.cs
@@ -33,41 +33,111 @@ namespace MarvelousReportMicroService.DAL.Repositories
 
         public List<Lead> GetLeadByParameters(LeadSearch lead)
         {
-            using IDbConnection connection = ProvideConnection();
-
             string nameParam = GenerateParamString.Generate(lead.NameParam, lead.Name);
             string lastNameParam = GenerateParamString.Generate(lead.LastNameParam, lead.LastName);
             string emailParam = GenerateParamString.Generate(lead.EmailParam, lead.Email);
             string phoneParam = GenerateParamString.Generate(lead.PhoneParam, lead.Phone);
 
-            return connection.
-                Query<Lead>(
-                Queries.GetLeadsByParameters
-                , new
-                {
-                    lead.Id,
-                    lead.StartBirthDate,
-                    lead.EndBirthDate,
-                    lead.Role,
-                    lead.IsBanned,
-                    NameParam = nameParam,
-                    LastNameParam = lastNameParam,
-                    EmailParam = emailParam,
-                    PhoneParam = phoneParam
-                }
-                , commandType: CommandType.StoredProcedure)
-                .ToList();
+            //return connection.
+            //    Query<Lead>(
+            //    Queries.GetLeadsByParameters
+            //    , new
+            //    {
+            //        lead.Id,
+            //        lead.StartBirthDate,
+            //        lead.EndBirthDate,
+            //        lead.Role,
+            //        lead.IsBanned,
+            //        NameParam = nameParam,
+            //        LastNameParam = lastNameParam,
+            //        EmailParam = emailParam,
+            //        PhoneParam = phoneParam
+            //    }
+            //    , commandType: CommandType.StoredProcedure)
+            //    .ToList();
 
-            //var phoneQuery = new Query().WhereLike("Phone", phoneParam);
-            //var emailQuery = new Query().WhereLike("Email", emailParam).From(phoneQuery);
-            //var lastNamelQuery = new Query().WhereLike("LastName", lastNameParam).From(emailQuery);
-            //var namelQuery = new Query().WhereLike("Name", nameParam).From(lastNamelQuery);
 
-            //var query = new Query().From(namelQuery);
+            var query = new Query("Lead");
 
-            //IEnumerable<Lead> leads = _qeryFactory.Query("Lead").Where("Id", 100).Get<Lead>();
+            if(lead.Id != null)
+            {
+                query = query.Where("Id", lead.Id);
+            }
 
-            //return (List<Lead>)leads;
+            if (lead.StartBirthDate != null && lead.EndBirthDate != null)
+            {
+                query = query
+                    .Where("BirthDay", ">", lead.StartBirthDate.Value.Day)
+                    .Where("BirthMonth", ">", lead.StartBirthDate.Value.Month)
+                    .Where("BirthYear", ">", lead.StartBirthDate.Value.Year)
+                    .Where("BirthDay", "<", lead.EndBirthDate.Value.Day)
+                    .Where("BirthMonth", "<", lead.EndBirthDate.Value.Month)
+                    .Where("BirthYear", "<", lead.EndBirthDate.Value.Year);
+            }
+
+            if (lead.Role != null)
+            {
+                query = query.Where("Role", lead.Role);
+            }
+
+            if (lead.IsBanned != null)
+            {
+                query = query.Where("IsBanned", lead.IsBanned);
+            }
+
+            if (lead.NameParam != null)
+            {
+                query = query.WhereLike("Name", nameParam);
+            }
+
+            if (lead.LastNameParam != null)
+            {
+                query = query.WhereLike("LastName", lastNameParam);
+            }
+
+            if (lead.EmailParam != null)
+            {
+                query = query.WhereLike("Email", emailParam);
+            }
+
+            if (lead.PhoneParam != null)
+            {
+                query = query.WhereLike("Phone", phoneParam);
+            }
+
+            query = query.As("Query");
+
+            //var phoneQuery = new Query("Lead").WhereLike("Phone", phoneParam).As("Email");
+            //var emailQuery = new Query("Phone").WhereLike("Email", emailParam).From(phoneQuery).As("Email");
+            //var lastNamelQuery = new Query("Email").WhereLike("LastName", lastNameParam).From(emailQuery).As("LastName");
+            //var namelQuery = new Query("LastName").WhereLike("Name", nameParam).From(lastNamelQuery).As("Name");
+
+
+            //var query = new Query("BirthDate")
+            //.Where(q =>
+            //    q.WhereNull("Id")
+            //    .OrWhereNotNull("Id")
+            //    .Where("Id", lead.Id))
+            //.Where(q =>
+            //    q.WhereNull("Role")
+            //    .OrWhereNotNull("Role")
+            //    .Where("Role", lead.Role))
+            //.Where(q =>
+            //    q.WhereNull("IsBanned")
+            //    .OrWhereNotNull("IsBanned")
+            //    .Where("IsBanned", lead.IsBanned))
+            //.Where(q =>
+            //    q.WhereNull("BirthDate")
+            //    .OrWhereNotNull("BirthDate")
+            //    .Where("BirthDate", ">", lead.StartBirthDate))
+            //.Where(q =>
+            //    q.WhereNull("BirthDate")
+            //    .OrWhereNotNull("BirthDate")
+            //    .Where("BirthDate", "<", lead.EndBirthDate))
+            //    .From(namelQuery).As("Query");
+            IEnumerable<Lead> leads = _qeryFactory.Query("Query").From(query).Get<Lead>();
+
+            return (List<Lead>)leads;
         }
 
 
@@ -85,6 +155,11 @@ namespace MarvelousReportMicroService.DAL.Repositories
                 }
                 , commandType: CommandType.StoredProcedure)
                 .ToList();
+        }
+
+        private Query GetSqlKataQurry(string table, string column, string param)
+        {
+            return new Query(table).WhereLike(column, param);
         }
     }
 }
