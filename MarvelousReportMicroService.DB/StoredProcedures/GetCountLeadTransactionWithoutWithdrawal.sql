ffe1fb64795ebd1b05ed27b40db5188fb44cfe54
AzarovRoman <azarovrom9215@gmail.com>
2022-03-28 19:13:51 +0300

    fix stored procedure

--- MarvelousReportMicroService.DB/StoredProcedures/GetCountLeadTransactionWithoutWithdrawal.sql
+++ MarvelousReportMicroService.DB/StoredProcedures/GetCountLeadTransactionWithoutWithdrawal.sql
@@ -5,4 +5,4 @@ AS
 	SELECT count(distinct t.[Date]) from [dbo].[Account] a
 	inner join [dbo].[Transaction] t
 	on t.AccountId = a.ExternalId
-	where a.LeadId = @LeadId and t.[Type] = 1 or t.[Type] = 3 or t.[Type] = 4
+	where a.LeadId = @LeadId and t.[Type] = 1 or t.[Type] = 3 or t.[Type] = 4 and t.[Date] >= @StartDate
