a85be2a47d096cbc025bb7abf5617915ab0206b8
AzarovRoman <azarovrom9215@gmail.com>
2022-03-28 18:34:51 +0300

    added stored procedure get BirthdayLead

---
+++ MarvelousReportMicroService.DB/StoredProcedures/GetBirthdayLead.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[GetBirthdayLead]
+	@Day tinyint,
+	@Month tinyint
+AS
+	SELECT l.ExternalId,
+		   l.[Name],
+		   l.LastName,
+		   --l.BitrhDate, --добавить это после мержа дева сюда
+		   l.Email,
+		   l.Phone,
+		   l.City
+	from [dbo].[Lead] l
+	where l.BirthDay = @Day and l.BirthMonth = @Month
