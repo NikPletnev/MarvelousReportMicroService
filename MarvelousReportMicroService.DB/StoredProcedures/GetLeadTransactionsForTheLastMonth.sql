be481d1246da24d7028ae4bf483d0bf73ad3a70d
Nikita <klophah@gmail.com>
2022-03-28 19:20:19 +0300

    fixed minor

--- MarvelousReportMicroService.DB/StoredProcedures/GetLeadTransactionsForTheLastMonth.sql
+++ MarvelousReportMicroService.DB/StoredProcedures/GetLeadTransactionsForTheLastMonth.sql
@@ -10,12 +10,11 @@ SELECT t.ExternalId,
 	   t.Currency,
 	   t.Rate,
 	   a.ExternalId as AccountId,
-	   l.ExternalId as LeadId
+	   a.LeadId
 FROM [ReportDb].[dbo].[Transaction] as t
 inner join [ReportDb].[dbo].[Account] as a on t.AccountId = a.ExternalId
-inner join [ReportDb].[dbo].[Lead] as l on a.LeadId = l.ExternalId
 WHERE t.Date >= DATEADD(month, -1, @startOfCurrentMonth) 
       AND t.Date < @startOfCurrentMonth
-	  AND l.ExternalId = @leadId
+	  AND a.LeadId = @leadId
 	  AND t.Type = 1
 	  AND t.Type = 2
