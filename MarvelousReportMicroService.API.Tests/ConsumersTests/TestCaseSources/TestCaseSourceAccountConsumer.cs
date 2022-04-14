5ce409faf7bcd33128d8d7a5af6ef6ab3022d508
Nikita <klophah@gmail.com>
2022-04-14 13:48:25 +0300

    Added first tests for Consumers

---
+++ MarvelousReportMicroService.API.Tests/ConsumersTests/TestCaseSources/TestCaseSourceAccountConsumer.cs
@@ -0,0 +1,38 @@
+﻿using Marvelous.Contracts.ExchangeModels;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarvelousReportMicroService.API.Tests.ConsumersTests
+{
+    internal class TestCaseSourceAccountConsumer : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            var accountExchangeModel = new AccountExchangeModel()
+            {
+                Id = 1,
+                Name = "TestAccOne",
+                CurrencyType = Marvelous.Contracts.Enums.Currency.RUB,
+                LeadId = 1,
+                IsBlocked = false
+            };
+            var accountExchangeModelSecond = new AccountExchangeModel()
+            {
+                Id = 2,
+                Name = "TestAccTwo",
+                CurrencyType = Marvelous.Contracts.Enums.Currency.GYD,
+                LeadId = 1,
+                IsBlocked = true
+            };
+
+
+            yield return new object[] { accountExchangeModel };
+            yield return new object[] { accountExchangeModelSecond };
+
+        }
+    }
+}
