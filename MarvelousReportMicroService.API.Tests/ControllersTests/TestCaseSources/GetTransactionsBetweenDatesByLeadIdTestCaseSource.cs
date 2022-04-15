62c8aa6a14218392776be19f96bf9ff58d2acdf3
Nikita <klophah@gmail.com>
2022-04-15 16:43:46 +0300

    minor update

---
+++ MarvelousReportMicroService.API.Tests/ControllersTests/TestCaseSources/GetTransactionsBetweenDatesByLeadIdTestCaseSource.cs
@@ -0,0 +1,99 @@
+﻿using Marvelous.Contracts.Enums;
+using Marvelous.Contracts.ResponseModels;
+using MarvelousReportMicroService.BLL.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarvelousReportMicroService.API.Tests.ControllersTests.TestCaseSources
+{
+    public class GetTransactionsBetweenDatesByLeadIdTestCaseSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            var leadId = 1;
+            var startDate  = new DateTime(1,1,1990);
+            var endDate = new DateTime(1,1,1991);
+            var transactionModels = new List<TransactionModel>
+            {
+                new TransactionModel()
+                {
+                    Id = 1,
+                    AccountId = 1,
+                    Amount = 100,
+                    Currency = Marvelous.Contracts.Enums.Currency.USD,
+                    Date = new DateTime(2,2,1990),
+                    Rate = 1000,
+                    Type = Marvelous.Contracts.Enums.TransactionType.Transfer
+                },
+                new TransactionModel()
+                {
+                    Id = 2,
+                    AccountId = 1,
+                    Amount = 100,
+                    Currency = Marvelous.Contracts.Enums.Currency.USD,
+                    Date = new DateTime(2,3,1990),
+                    Rate = 1000,
+                    Type = Marvelous.Contracts.Enums.TransactionType.Transfer
+                },
+                new TransactionModel()
+                {
+                    Id = 3,
+                    AccountId = 1,
+                    Amount = 100,
+                    Currency = Marvelous.Contracts.Enums.Currency.USD,
+                    Date = new DateTime(2,2,1994),
+                    Rate = 1000,
+                    Type = Marvelous.Contracts.Enums.TransactionType.Transfer
+                }
+            };
+
+            var expectedTransactionModels = new List<TransactionModel>
+            {
+                new TransactionModel()
+                {
+                    Id = 1,
+                    AccountId = 1,
+                    Amount = 100,
+                    Currency = Marvelous.Contracts.Enums.Currency.USD,
+                    Date = new DateTime(2,2,1990),
+                    Rate = 1000,
+                    Type = Marvelous.Contracts.Enums.TransactionType.Transfer
+                },
+                new TransactionModel()
+                {
+                    Id = 2,
+                    AccountId = 1,
+                    Amount = 100,
+                    Currency = Marvelous.Contracts.Enums.Currency.USD,
+                    Date = new DateTime(2,3,1990),
+                    Rate = 1000,
+                    Type = Marvelous.Contracts.Enums.TransactionType.Transfer
+                }
+            };
+
+            IdentityResponseModel model = new IdentityResponseModel()
+            {
+                Id = 1,
+                Role = "role",
+                IssuerMicroservice = Microservice.MarvelousAuth.ToString()
+            };
+
+
+            yield return new object[] { leadId, startDate, endDate, transactionModels, model, expectedTransactionModels };
+
+
+
+        }
+    }
+}
+
+
+//int leadId,
+//            DateTime startDate,
+//            DateTime endDate,
+//            List<TransactionModel> transactionModels,
+//            IdentityResponseModel model
--- MarvelousReportMicroService.API.Tests/ControllersTests/TransactionControllerTests.cs
+++ MarvelousReportMicroService.API.Tests/ControllersTests/TransactionControllerTests.cs
@@ -1,15 +1,19 @@
 ﻿using AutoMapper;
+using Marvelous.Contracts.ResponseModels;
 using MarvelousReportMicroService.API.Configuration;
 using MarvelousReportMicroService.API.Controllers;
 using MarvelousReportMicroService.API.Models;
 using MarvelousReportMicroService.API.Tests.ConsumersTests;
 using MarvelousReportMicroService.API.Tests.ControllersTests.TestCaseSources;
+using MarvelousReportMicroService.BLL.Helpers;
 using MarvelousReportMicroService.BLL.Models;
 using MarvelousReportMicroService.BLL.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Moq;
 using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -19,6 +23,8 @@ namespace MarvelousReportMicroService.API.Tests.ControllersTests
     {
         private Mock<ITransactionService> _transactionServiceMock;
         private TransactionsController _transactionController;
+        private const string jwtToken = "testToken";
+        private Mock<IRequestHelper> _requestHelperMock;
 
         [SetUp]
         public void Setup()
@@ -26,48 +32,46 @@ namespace MarvelousReportMicroService.API.Tests.ControllersTests
             _mapper = new Mapper(new MapperConfiguration(cfg => cfg.AddProfile<APIMapper>()));
             _transactionServiceMock = new Mock<ITransactionService>();
             _logger = new Mock<ILogger<TransactionsController>>();
+            _requestHelperMock = new Mock<IRequestHelper>();
             _transactionController = new TransactionsController(
                 _mapper,
                 _logger.Object,
                 _transactionServiceMock.Object);
         }
 
-        [Test]
-        public async Task GetTransactionsBetweenDatesByLeadIdTest_ShouldRetrurnTransactionsBetweenDates()
+        [TestCaseSource(typeof(GetTransactionsBetweenDatesByLeadIdTestCaseSource))]
+        public async Task GetTransactionsBetweenDatesByLeadIdTest_ShouldRetrurnTransactions
+            (int leadId, 
+            DateTime startDate, 
+            DateTime endDate, 
+            List<TransactionModel> transactionModels,
+            IdentityResponseModel model,
+            List<TransactionModel> expectedTransactionModels)
         {
             //given
+            var context = new DefaultHttpContext();
+            context.Request.Headers.Authorization = jwtToken;
+            _transactionController.ControllerContext.HttpContext = context;
+
+            _requestHelperMock.Setup(x => x.SendRequestCheckValidateToken(
+                It.IsAny<string>(),
+                It.IsAny<string>(),
+                It.IsAny<string>()))
+                .ReturnsAsync(model);
+
+            _transactionServiceMock.Setup(l => l.GetTransactionsBetweenDatesByLeadId(leadId, startDate, endDate)).ReturnsAsync(transactionModels);
 
             //when
+            var result = await _transactionController.GetTransactionsBetweenDatesByLeadId(leadId, startDate, endDate);
 
 
             //then
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<OkObjectResult>(result.Result);
+
         }
 
 
 
-
-
-        //[Test]
-        //public async Task GetAccountBalanceByAccountId_Returns200()
-        //{
-        //    //given
-        //    int id = 42;
-        //    decimal balance = 15;
-
-        //    string firstMessage = $"Request to get account balance with id = {id}";
-        //    string secondMessage = $"Answer to a request to get the balance of an account" +
-        //        $" with id = {id}, balance = {balance}";
-
-        //    _accountServiceMock.Setup(a => a.GetAccountBalance(id)).ReturnsAsync(balance);
-
-        //    //when
-        //    var result = await _transactionController.GetAccountBalance(id);
-
-        //    //then
-        //    Assert.AreEqual(balance, result);
-        //    //Assert.IsInstanceOf<OkObjectResult>(result.Result);
-        //    VerifyLogger(LogLevel.Information, firstMessage);
-        //    VerifyLogger(LogLevel.Information, secondMessage);
-        //}
     }
 }
