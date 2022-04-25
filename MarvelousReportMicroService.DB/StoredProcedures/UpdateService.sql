a705ab78b6775115cabf52cf81dad4eaa2fb62a0
AzarovRoman <azarovrom9215@gmail.com>
2022-04-25 19:56:33 +0300

    Added updateService and addService stored procedures

---
+++ MarvelousReportMicroService.DB/StoredProcedures/UpdateService.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[UpdateService]
+	@ExternalId int,
+	@Name varchar,
+	@Type int,
+	@Description varchar,
+	@Price decimal,
+	@IsDeleted bit
+AS
+update [dbo].[Service]
+set 
+[Name] = @Name,
+[Type] = @Type,
+[Description] = @Description,
+[Price] = @Price,
+[IsDeleted] = @IsDeleted
+Where ExternalId = @ExternalId
