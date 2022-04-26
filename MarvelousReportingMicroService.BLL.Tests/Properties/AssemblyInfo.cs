51f4040a3d95e0f1e37ffc510cd50c33723dbb6f
AzarovRoman <azarovrom9215@gmail.com>
2022-04-26 14:09:12 +0300

    created bll.tests project

---
+++ MarvelousReportingMicroService.BLL.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MarvelousReportingMicroService.BLL.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MarvelousReportingMicroService.BLL.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e94a6947-c356-4f7a-ba4d-fd762c2f8354")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MarvelousReportingMicroService.BLL.Tests/ServiceServiceTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MarvelousReportingMicroService.BLL.Tests
+{
+    [TestClass]
+    public class ServiceServiceTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
