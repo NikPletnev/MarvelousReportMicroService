01596bbd40edfba7d468ae8b675ddaaa797cb042
AzarovRoman <azarovrom9215@gmail.com>
2022-04-27 18:42:18 +0300

    added test for TransactionFeeConsumer

---
+++ MarvelousReportMicroService.API.Tests/ConsumersTests/TestCaseSources/AddTransactionFeeTestCaseSource.cs
@@ -0,0 +1,19 @@
+﻿using Marvelous.Contracts.ExchangeModels;
+using System.Collections;
+
+namespace MarvelousReportMicroService.API.Tests.ConsumersTests.TestCaseSources
+{
+    internal class AddTransactionFeeTestCaseSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            var TransactionFeeExchangeModel = new ComissionTransactionExchangeModel()
+            {
+                IdTransaction = 1,
+                AmountComission = 33.12m
+            };
+
+            yield return TransactionFeeExchangeModel;
+        }
+    }
+}
